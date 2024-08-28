a2d701543aa9c2bcd1bd60a1d156c0be0083d735
janisfs <janisfs@mail.ru>
2024-08-28 12:15:33 +0300

    add new code

--- divan_project/divan_project/spiders/divans_spider.py
+++ divan_project/divan_project/spiders/divans_spider.py
@@ -7,4 +7,12 @@ class DivansSpiderSpider(scrapy.Spider):
     start_urls = ["https://www.divan.ru/category/divany-i-kresla"]
 
     def parse(self, response):
-        pass
+        divans = response.css('div._Ud0k')
+        for divan in divans:
+            yield {
+                "name": divan.css("div.wYUX2 span::text").get(),
+                "price": divan.css("div.pY3d2 span::text").get(),
+                "url": response.urljoin(divan.css("a").attrib["href"])
+            }
+
+
